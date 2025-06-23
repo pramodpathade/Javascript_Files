cf28ad742441d5cc1202668653d1dbbc5cf1a9dc
Pathade Pramod <pramodpathade735@gmail.com>
2025-06-23 20:44:20 +0530

    added new js/html file

---
+++ 76.main.js
@@ -0,0 +1,9 @@
+import { add, sub, mul, div } from './77.mymodule.js';
+
+const a = 20;
+const b = 30;
+
+console.log("Addition is: " + add(a, b));
+console.log("Subtraction is: " + sub(a, b));
+console.log("Multiplication is: " + mul(a, b));
+console.log("Division is: " + div(a, b));
